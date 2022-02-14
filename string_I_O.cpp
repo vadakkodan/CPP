d2fc9a47a4e45533dae59ed3cb765a1182a537f0
vadakkodan <pradeepvadakkodathu@gmail.com>
2022-02-14 17:24:13 +0530

    sting input output

---
+++ string_I_O.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int main() {
+	char a[20];
+	std::cout <<"Enter your name:\n";
+	std::cin >> a;
+	std::cout<<"Welcome," << a <<"!\n";
+	return 0;
+}
+
