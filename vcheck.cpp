d2d616a486e540cc24df8c85ccb57357194d9e91
vadakkodan <pradeepvadakkodathu@gmail.com>
2022-02-14 17:27:03 +0530

    git restore check

--- vcheck.cpp
+++ vcheck.cpp
@@ -1,12 +1,7 @@
 #include<iostream>
-#include<cstring>
-using namespace std;
 
 int main() {
-	char a[20];
-	std::cout <<"Enter your name:\n";
-	std::cin >> a;
-	std::cout<<"Welcome," << a <<"!\n";
-	return 0;
+	std::cout <<"OM Hari Sree Ganapathe Namaha:\n";
+		return 0;
 }
 
